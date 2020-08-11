1aed688e1c8e0f269ec0b21df172a23689f6f4f3
stainesjeba <69515024+stainesjeba@users.noreply.github.com>
2020-08-11 20:56:35 +0530

    Update Db_Connection.php

--- Login/Db_Connection.php
+++ Login/Db_Connection.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = new mysqli("Localhost","root","","Event Creator");
+$conn = new mysqli("ec2-50-16-198-4.compute-1.amazonaws.com","gqifynvjlcfhmv","81596a351b90841695300202741d23445e97d3033f4b8fd88fba488c169d967c","d40d564tipoee1");
 
 ?>
